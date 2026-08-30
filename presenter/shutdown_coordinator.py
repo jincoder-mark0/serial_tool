b8b610c0c0cd34d27817b7094871a13deb2d6f44
kjlee-inlct <kjlee@inlct.com>
2026-08-30 14:45:09 +0900

    Refactor: ShutdownCoordinator가 PortScanManager를 직접 종료

--- presenter/shutdown_coordinator.py
+++ presenter/shutdown_coordinator.py
@@ -1,7 +1,7 @@
 """
-애플리케이션 종료 시퀀스 조정 모듈
+애플리케이션 종료 시퀀스 조정 모듈.
 
-MainPresenter에서 백그라운드 작업 종료, 상태 저장, 연결 종료, logger drain 순서를 분리합니다.
+백그라운드 작업 종료, 상태 저장, 연결 종료, logger drain 순서를 관리합니다.
 S-059의 데이터 보존 순서(connection close -> processEvents -> logger stop)를 보존합니다.
 """
 from typing import Callable, Optional
@@ -13,10 +13,10 @@ from core.logger import logger
 from core.settings_manager import SettingsManager
 from model.connection_controller import ConnectionController
 from model.macro_runner import MacroRunner
+from model.port_scan_manager import PortScanManager
 from presenter.data_handler import DataTrafficHandler
-from presenter.packet_presenter import PacketPresenter
-from presenter.port_presenter import PortPresenter
 from presenter.manual_control_presenter import ManualControlPresenter
+from presenter.packet_presenter import PacketPresenter
 from presenter.shutdown_state_collector import ShutdownStateCollector
 from view.main_window import MainWindow
 
@@ -30,7 +30,7 @@ class ShutdownCoordinator:
         settings_manager: SettingsManager,
         connection_controller: ConnectionController,
         macro_runner: MacroRunner,
-        port_presenter: PortPresenter,
+        port_scan_manager: PortScanManager,
         manual_control_presenter: ManualControlPresenter,
         packet_presenter: PacketPresenter,
         data_handler: DataTrafficHandler,
@@ -41,7 +41,7 @@ class ShutdownCoordinator:
         self._settings_manager = settings_manager
         self._connection_controller = connection_controller
         self._macro_runner = macro_runner
-        self._port_presenter = port_presenter
+        self._port_scan_manager = port_scan_manager
         self._manual_control_presenter = manual_control_presenter
         self._packet_presenter = packet_presenter
         self._data_handler = data_handler
@@ -57,7 +57,7 @@ class ShutdownCoordinator:
             self._macro_runner.stop()
             self._macro_runner.wait(1000)
 
-        self._port_presenter.stop_pending_scan()
+        self._port_scan_manager.stop()
         self._data_handler.stop()
         self._packet_presenter.stop()
 
@@ -70,7 +70,7 @@ class ShutdownCoordinator:
         if self._connection_controller.has_active_connection:
             self._connection_controller.close_connection()
 
-        # S-059: Worker가 종료 직전 emit한 queued RX를 main thread에서 먼저 전달한다.
+        # S-059: Worker가 종료 직전 emit한 queued RX를 main thread에서 먼저 전달합니다.
         QCoreApplication.processEvents()
         data_logger_manager.stop_all()
         logger.info("Shutdown completed.")
