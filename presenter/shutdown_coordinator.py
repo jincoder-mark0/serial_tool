e5b70e4bce0e4bff5d1a48139ccad58a21f89077
kjlee-inlct <kjlee@inlct.com>
2026-08-30 15:37:09 +0900

    refactor: stop StatusCoordinator during shutdown

--- presenter/shutdown_coordinator.py
+++ presenter/shutdown_coordinator.py
@@ -4,9 +4,9 @@
 백그라운드 작업 종료, 상태 저장, 연결 종료, logger drain 순서를 관리합니다.
 S-059의 데이터 보존 순서(connection close -> processEvents -> logger stop)를 보존합니다.
 """
-from typing import Callable, Optional
+from typing import Callable
 
-from PyQt5.QtCore import QCoreApplication, QTimer
+from PyQt5.QtCore import QCoreApplication
 
 from core.data_logger import data_logger_manager
 from core.logger import logger
@@ -20,6 +20,7 @@ from presenter.data_handler import DataTrafficHandler
 from presenter.manual_control_presenter import ManualControlPresenter
 from presenter.packet_presenter import PacketPresenter
 from presenter.shutdown_state_collector import ShutdownStateCollector
+from presenter.status_coordinator import StatusCoordinator
 from view.main_window import MainWindow
 
 
@@ -39,7 +40,7 @@ class ShutdownCoordinator:
         packet_presenter: PacketPresenter,
         data_handler: DataTrafficHandler,
         close_system_log: Callable[[], None],
-        status_timer: Optional[QTimer] = None,
+        status_coordinator: StatusCoordinator,
     ) -> None:
         self._view = view
         self._settings_manager = settings_manager
@@ -52,7 +53,7 @@ class ShutdownCoordinator:
         self._packet_presenter = packet_presenter
         self._data_handler = data_handler
         self._close_system_log = close_system_log
-        self._status_timer = status_timer
+        self._status_coordinator = status_coordinator
 
     def shutdown(self) -> None:
         """백그라운드 작업, 상태 저장, 연결 및 logger를 안전한 순서로 종료합니다."""
@@ -69,9 +70,7 @@ class ShutdownCoordinator:
         self._port_scan_manager.stop()
         self._data_handler.stop()
         self._packet_presenter.stop()
-
-        if self._status_timer:
-            self._status_timer.stop()
+        self._status_coordinator.stop()
 
         self._close_system_log()
         self._save_ui_state()
