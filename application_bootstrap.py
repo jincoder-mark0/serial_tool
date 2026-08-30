91d2ab90e3afcafdc0fd8297c4debad015502cfc
kjlee-inlct <kjlee@inlct.com>
2026-08-30 15:52:24 +0900

    refactor: expose internal data handler only on app runtime graph

--- application_bootstrap.py
+++ application_bootstrap.py
@@ -38,6 +38,7 @@ class ApplicationComponents:
 
     main_presenter_dependencies: MainPresenterDependencies
     connection_controller: ConnectionController
+    data_handler: DataTrafficHandler
     file_transfer_manager: FileTransferManager
     port_scan_manager: PortScanManager
     macro_script_manager: MacroScriptManager
@@ -58,23 +59,14 @@ class ApplicationBootstrapper:
 
         packet_parser_manager = PacketParserManager()
         connection_session_factory = ConnectionSessionFactory()
-        connection_controller = ConnectionController(
-            packet_parser_manager,
-            connection_session_factory,
-        )
-        command_transmission_service = CommandTransmissionService(
-            connection_controller,
-            self._settings_manager,
-        )
+        connection_controller = ConnectionController(packet_parser_manager, connection_session_factory)
+        command_transmission_service = CommandTransmissionService(connection_controller, self._settings_manager)
         file_transfer_manager = FileTransferManager(connection_controller)
         port_scan_manager = PortScanManager()
         macro_runner = MacroRunner()
         macro_script_manager = MacroScriptManager()
         macro_execution_coordinator = MacroExecutionCoordinator(
-            macro_runner,
-            connection_controller,
-            command_transmission_service,
-            self._view.port_view,
+            macro_runner, connection_controller, command_transmission_service, self._view.port_view
         )
         traffic_monitor = TrafficMonitor()
         data_handler = DataTrafficHandler(self._view, traffic_monitor)
@@ -82,21 +74,12 @@ class ApplicationBootstrapper:
         status_coordinator = StatusCoordinator(self._view, traffic_monitor)
 
         port_presenter = PortPresenter(
-            self._view.port_view,
-            connection_controller,
-            self._settings_manager,
-            port_scan_manager,
+            self._view.port_view, connection_controller, self._settings_manager, port_scan_manager
         )
-        macro_presenter = MacroPresenter(
-            self._view.macro_view,
-            macro_runner,
-            macro_script_manager,
-        )
+        macro_presenter = MacroPresenter(self._view.macro_view, macro_runner, macro_script_manager)
         file_presenter = FilePresenter(file_transfer_manager)
         packet_presenter = PacketPresenter(
-            self._view.packet_view,
-            connection_controller,
-            self._settings_manager,
+            self._view.packet_view, connection_controller, self._settings_manager
         )
         manual_control_presenter = ManualControlPresenter(
             self._view.manual_control_view,
@@ -105,13 +88,10 @@ class ApplicationBootstrapper:
             command_transmission_service,
         )
 
-        # 변하지 않는 data path는 composition root에서 한 번만 배선합니다.
         connection_controller.data_received.connect(data_handler.on_fast_data_received)
         connection_controller.data_sent.connect(data_handler.on_data_sent)
         connection_controller.data_received.connect(macro_runner.on_data_received)
-        manual_control_presenter.local_echo_requested.connect(
-            self._view.append_local_echo_data
-        )
+        manual_control_presenter.local_echo_requested.connect(self._view.append_local_echo_data)
 
         shutdown_coordinator = ShutdownCoordinator(
             view=self._view,
@@ -147,6 +127,7 @@ class ApplicationBootstrapper:
         return ApplicationComponents(
             main_presenter_dependencies=main_presenter_dependencies,
             connection_controller=connection_controller,
+            data_handler=data_handler,
             file_transfer_manager=file_transfer_manager,
             port_scan_manager=port_scan_manager,
             macro_script_manager=macro_script_manager,
